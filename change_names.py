7633486a8b3d562b7ea7514a9f229be0b9b9121f
Debidatta Ray <debidattaray@gmail.com>
2023-09-13 11:26:21 +0530

    first commit...

--- change_names.py
+++ change_names.py
@@ -1,9 +1,9 @@
-ip_address = '15.204.9.254'
-domainname = 'channel18news.com'
+ip_address = 'chane_ip_address_here'
+domainname = 'give_godaddy_domain.com'
 # add spf and dkim domain names
 sub_domain = 'subdomain'
 smtp_username = 'smtp_username'
 smtp_password = 'smtp_password'
 # pmta_config_path = "/etc/pmta/config"
-rdns_record_name = 'any_name'
+rdns_record_name = 'retro'
 # ip_address = 
